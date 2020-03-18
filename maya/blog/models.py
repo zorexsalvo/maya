4e299cae9582785fffd42e2f8d06a5109962ea32
Zorex Salvo <zorexsalvo@gmail.com>
2020-03-18 21:48:43 +0800

    Create blog page tag model

--- maya/blog/models.py
+++ maya/blog/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 from modelcluster.fields import ParentalKey
+from modelcluster.tags import ClusterTaggableManager
+
+from taggit.models import TaggedItemBase
 
 from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 
 from wagtail.search import index
@@ -25,10 +28,15 @@ class BlogIndexPage(Page):
         return context
 
 
+class BlogPageTag(TaggedItemBase):
+    content = ParentalKey('BlogPage', on_delete=models.CASCADE, related_name='tagged_items')
+
+
 class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
 
     def main_image(self):
         image = self.gallery_images.first()
@@ -43,7 +51,10 @@ class BlogPage(Page):
     ]
 
     content_panels = Page.content_panels + [
-        FieldPanel('date'),
+        MultiFieldPanel([
+            FieldPanel('date'),
+            FieldPanel('tags'),
+        ]),
         FieldPanel('intro'),
         FieldPanel('body', classname='full'),
         InlinePanel('gallery_images', label="Gallery Images")
